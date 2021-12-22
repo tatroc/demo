538af721fc77b613d0e7e7569973f3fb36cf88b3
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-22 07:29:11 -0600

    new deployment

--- script.sh
+++ script.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-git diff HEAD~1 --name-only | grep '/' | awk -F '/' '{ print $1 }' | head -n 1
+DIR=$(git diff HEAD~1 --name-only | grep '/' | awk -F '/' '{ print $1 }' | head -n 1)
+echo $DIR
+
+
+
+
+version=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+NEW_VERSION=$(echo $version | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
+echo "new tag $NEW_VERSION"
+git tag $NEW_VERSION
+
+echo "list tags"
+git tag
