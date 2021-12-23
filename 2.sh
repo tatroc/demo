b465ef9966286376bfb228e6b7a16993a19a822a
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-23 14:15:42 -0600

    test

--- 2.sh
+++ 2.sh
@@ -1,71 +1,45 @@
-#!/bin/bash
-
 HASH_FILE="commit_hashes.txt"
-ALL_COMMIT_HASH_FILE="./all_commit_hashes.txt"
+ALL_COMMIT_HASH_FILE="all_commit_hashes.txt"
 
 COMMITTED_UNTAGGED_FILES="committed_untagged_files.txt"
 
-if [ -f $HASH_FILE ] ; then
-    rm $HASH_FILE
-fi
-if [ -f $ALL_COMMIT_HASH_FILE ] ; then
-    rm $ALL_COMMIT_HASH_FILE
-fi
-if [ -f $COMMITTED_UNTAGGED_FILES ] ; then
-    rm $COMMITTED_UNTAGGED_FILES
+function getmoduledirectories {
+
+WORK_DIR=$(mktemp -d -p "/tmp")
+if [[ ! "${WORK_DIR}" || ! -d "${WORK_DIR}" ]]; then
+  echo "Could not create temp dir"
+  exit 1
 fi
 
-#NOT_PUSHED=$(git push --dry-run --porcelain | sed -n '2 p' | awk '{ print $2 }')
+git log --pretty="%D %H" --decorate=short --decorate-refs=refs/tags > "${WORK_DIR}/${ALL_COMMIT_HASH_FILE}"
 
-git log --pretty="%D %H" --decorate=short --decorate-refs=refs/tags > $ALL_COMMIT_HASH_FILE
-
-
-
-
-exec 4<"$ALL_COMMIT_HASH_FILE"
+exec 4<"${WORK_DIR}/${ALL_COMMIT_HASH_FILE}"
 echo Start
 while read -u4 line ; do
     echo "$line"
 
     if [[ "$line" != *"tag"* ]]; then
         echo "It's there. $line"
-        echo $line >> $HASH_FILE
+        echo $line >> "${WORK_DIR}/${HASH_FILE}"
     else
         echo "tag found in line $line"
         break
     fi
 done
 
-cat ./$HASH_FILE
+cat "${WORK_DIR}/${HASH_FILE}"
 
-
-#DIRECTORY_LIST=$(git show --pretty="format:" --name-only $NOT_PUSHED | awk NF | grep '/' | awk -F '/' '{ print $1 }' | sort -u)
-
-
-#DIRECTORY_LIST=($(git diff HEAD~1 --name-only | grep '/' | awk -F '/' '{ print $1 }' | sort -u))
-#declare -p DIRECTORY_LIST
-
-#echo "Working with directories ${DIRECTORY_LIST[@]}"
-
-#for dir in "${DIRECTORY_LIST[@]}"
-#do
-#    echo "item"
-#    echo "${dir}"
-#done
-
-exec 4<"$HASH_FILE"
+exec 4<"${WORK_DIR}/${HASH_FILE}"
 echo Start
 while read -u4 line ; do
     echo "$line"
 
-    git show --pretty="format:" --name-only $line >> $COMMITTED_UNTAGGED_FILES
+    git show --pretty="format:" --name-only $line >> "${WORK_DIR}/${COMMITTED_UNTAGGED_FILES}"
 done
 
+cat "${WORK_DIR}/${COMMITTED_UNTAGGED_FILES}"
 
-cat $COMMITTED_UNTAGGED_FILES
-
-
-DIRECTORY_LIST=($(cat $COMMITTED_UNTAGGED_FILES | grep '/' | awk -F '/' '{ print $1 }' | sort -u))
+DIRECTORY_LIST=($(cat ${WORK_DIR}/${COMMITTED_UNTAGGED_FILES} | grep '/' | awk -F '/' '{ print $1 }' | sort -u))
 declare -p DIRECTORY_LIST
 
 echo "Working with directories ${DIRECTORY_LIST[@]}"
@@ -75,3 +49,11 @@ do
     echo "item"
     echo "${dir}"
 done
+
+
+echo "Working with TF module directories ${DIRECTORY_LIST[@]}"
+
+echo $WORK_DIR
+}
+
+getmoduledirectories $HASH_FILE $ALL_COMMIT_HASH_FILE $COMMITTED_UNTAGGED_FILES
--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 HASH_FILE="commit_hashes.txt"
-ALL_COMMIT_HASH_FILE="./all_commit_hashes.txt"
+ALL_COMMIT_HASH_FILE="all_commit_hashes.txt"
 
 COMMITTED_UNTAGGED_FILES="committed_untagged_files.txt"
 
-WORK_DIR=$(mktemp -d -p "$DIR")
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+function getmoduledirectories {
+
+WORK_DIR=$(mktemp -d -p "/tmp")
+if [[ ! "${WORK_DIR}" || ! -d "${WORK_DIR}" ]]; then
   echo "Could not create temp dir"
   exit 1
 fi
 
-git log --pretty="%D %H" --decorate=short --decorate-refs=refs/tags > $WORK_DIR/$ALL_COMMIT_HASH_FILE
+git log --pretty="%D %H" --decorate=short --decorate-refs=refs/tags > "${WORK_DIR}/${ALL_COMMIT_HASH_FILE}"
 
-exec 4<"$WORK_DIR/$ALL_COMMIT_HASH_FILE"
+exec 4<"${WORK_DIR}/${ALL_COMMIT_HASH_FILE}"
 echo Start
 while read -u4 line ; do
     echo "$line"
 
     if [[ "$line" != *"tag"* ]]; then
         echo "It's there. $line"
-        echo $line >> $HASH_FILE
+        echo $line >> "${WORK_DIR}/${HASH_FILE}"
     else
         echo "tag found in line $line"
         break
     fi
 done
 
-cat $WORK_DIR/$HASH_FILE
+cat "${WORK_DIR}/${HASH_FILE}"
 
-exec 4<"$WORK_DIR/$HASH_FILE"
+exec 4<"${WORK_DIR}/${HASH_FILE}"
 echo Start
 while read -u4 line ; do
     echo "$line"
 
-    git show --pretty="format:" --name-only $line >> $WORK_DIR/$COMMITTED_UNTAGGED_FILES
+    git show --pretty="format:" --name-only $line >> "${WORK_DIR}/${COMMITTED_UNTAGGED_FILES}"
 done
 
-cat $COMMITTED_UNTAGGED_FILES
+cat "${WORK_DIR}/${COMMITTED_UNTAGGED_FILES}"
 
-DIRECTORY_LIST=($(cat $COMMITTED_UNTAGGED_FILES | grep '/' | awk -F '/' '{ print $1 }' | sort -u))
+DIRECTORY_LIST=($(cat ${WORK_DIR}/${COMMITTED_UNTAGGED_FILES} | grep '/' | awk -F '/' '{ print $1 }' | sort -u))
 declare -p DIRECTORY_LIST
 
 echo "Working with directories ${DIRECTORY_LIST[@]}"
@@ -48,6 +50,14 @@ do
     echo "item"
     echo "${dir}"
 done
+
+
+#echo "Working with TF module directories ${DIRECTORY_LIST[@]}"
+
+echo $WORK_DIR
+}
+
+getmoduledirectories $HASH_FILE $ALL_COMMIT_HASH_FILE $COMMITTED_UNTAGGED_FILES
 
 
 echo "Working with TF module directories ${DIRECTORY_LIST[@]}"
