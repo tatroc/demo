ab74ab47a744ddfebee7a0b4f1394612f7e93f05
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-25 11:22:53 -0600

    test

--- sbx.env.groovy
+++ sbx.env.groovy
@@ -1,6 +1,7 @@
 
 SCM_OWNER="tatroc"
 SCM_REPO="demo"
+SCM_REPO2="demo"
 SCM_URL="https://github.com/${SCM_OWNER}/${SCM_REPO}.git"
 MVN_URL="https://maven.pkg.github.com/${SCM_OWNER}/${SCM_REPO}"
 GIT_CRED_ID="tatroc_gh"
