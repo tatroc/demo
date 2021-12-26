9ddb4435136c8684ae06be8086f3e6b635038f54
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-26 17:33:54 -0600

    test

--- prepare.sh
+++ prepare.sh
@@ -6,12 +6,12 @@ export DEBIAN_FRONTEND="noninteractive"
 env
 mkdir -p ~/.m2
 cp ./mvn-settings.xml ~/.m2/settings.xml
-dpkg -s maven || EXIT_CODE=$?
-if [ $EXIT_CODE -eq 1 ]; then
+#dpkg -s maven || EXIT_CODE=$?
+#if [ $EXIT_CODE -eq 1 ]; then
     echo "Installing maven"
     apt update
-    apt install -y maven
-fi
+    apt install -y maven git
+#fi
 
 export JAVA_HOME=/usr
 export M2_HOME=/usr/share/maven
