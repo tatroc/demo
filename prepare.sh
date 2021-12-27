fd12c11829c87cc1d2ce946b4550ce89931c8d3a
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-27 06:53:22 -0600

    test

--- prepare.sh
+++ prepare.sh
@@ -9,6 +9,8 @@ cp ./mvn-settings.xml ~/.m2/settings.xml
 #dpkg -s maven || EXIT_CODE=$?
 #if [ $EXIT_CODE -eq 1 ]; then
     echo "Installing maven"
+    apt install software-properties-common
+    apt update
     add-apt-repository ppa:git-core/ppa -y
     apt update
     apt install -t buster-backports git
