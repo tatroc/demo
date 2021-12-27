cf986adec50fc7ef04ea6a50af0910a2c0be3fdf
Chris Tatro <christopher.tatro@kaplan.edu>
2021-12-27 10:51:37 -0600

    test

--- cosmosdb_private/main.tf
+++ cosmosdb_private/main.tf
@@ -1,4 +1,4 @@
-# My comment 25
+# My comment 26
 locals {
   tags            = merge(var.tags, {"PROVISIONED_BY" = "KNA CloudOps Terraform", "CONTACT" = "tss-devops@kaplan.com"})
   subnet_id       = var.subnet_id
