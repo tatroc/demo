a06e32d3944eb284c40e408a8ecb4db8453c7706
Chris Tatro <christopher.tatro@kaplan.edu>
2022-02-07 11:36:29 -0600

    update storage account tags

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -1,4 +1,12 @@
 #
+
+locals {
+
+  tags            = merge(var.tags, {"PROVISIONED_BY" = "KNA CloudOps Terraform 2", "CONTACT" = "devops@domain.com", "OWNER" = "christopher.tatro@kaplan.edu", "VERSION" = "v133"})
+
+}
+
+
 resource "azurerm_storage_account" "sg" {
   name                              = var.name
   resource_group_name               = var.resource_group
@@ -6,5 +14,5 @@ resource "azurerm_storage_account" "sg" {
   account_tier                      = var.account_tier
   account_replication_type          = var.account_replication_type
   #enable_advanced_threat_protection = "true"
-  tags                              = var.tags
+  tags                              = local.tags
 }
